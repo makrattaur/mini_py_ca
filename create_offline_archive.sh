06e287267ddcb8f113c51729a033278095d81068
Michael Tran <mike_tran@live.com>
2019-11-26 13:09:14 -0500

    Add check for directory presence in offline archive maker

--- create_offline_archive.sh
+++ create_offline_archive.sh
@@ -2,6 +2,10 @@
 
 pip3 download -r requirements.txt -d offline_deps
 
+if [ ! -d offline_packs ] ; then
+	mkdir offline_packs
+fi
+
 currentVer="$(git rev-parse --short HEAD)"
 tar jcvf "offline_packs/mca_${currentVer}.tar.bz2" dist/mini_py_ca-0.0.1+${currentVer}* offline_deps install_offline.sh
 
